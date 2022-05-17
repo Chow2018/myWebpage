d152d392339051abe9d0dba696c3034451d6d5eb
Chow2018 <wmchow2005@yahoo.com.hk>
2022-05-17 23:18:53 +0800

    202205162318

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash
 from youtube_subtitle import getYouTubeTranscript
 from test import getSomeText
+import json
 
 app = Flask(__name__)
 
@@ -11,3 +12,11 @@ def index():
 @app.route("/youtubeSubtitle")
 def youtubeSubtitle():
     return getYouTubeTranscript("Ngk09xRRzW8")
+
+@app.route("/youtubeSubtitle2/<url>")
+def youtubeSubtitle2(url):
+    subtitle = {
+        "url": url,
+        "text": getYouTubeTranscript(url)    
+    } 
+    return json.dumps(subtitle)
